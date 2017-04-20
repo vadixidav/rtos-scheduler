4ed85b5b9da428ba489426ca64af9be66f27376a
Geordon Worley <vadixidav@gmail.com>
2017-04-20 14:19:17 -0400

    add cyclic load and save

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ struct Task {
     wcet: f64,
 }
 
+struct Async {
+    task: Task,
+    release: f64,
+    priority: usize,
+}
+
 struct Cyclic {
     task: Task,
     period: f64,
@@ -16,7 +22,39 @@ impl Cyclic {
     }
 }
 
-fn response_time_rate_monotonic(mut tasks: Vec<Cyclic>) -> Vec<f64> {
+struct Event {
+    start: f64,
+    end: f64,
+}
+
+impl Event {
+    fn new(start: f64, end: f64) -> Event {
+        Event {
+            start: start,
+            end: end,
+        }
+    }
+}
+
+struct ScheduleEvent {
+    task: usize,
+    event: Event,
+}
+
+impl ScheduleEvent {
+    fn new(task: usize, start: f64, end: f64) -> ScheduleEvent {
+        ScheduleEvent {
+            task: task,
+            event: Event::new(start, end),
+        }
+    }
+}
+
+struct ReleaseSchedule {
+    tasks: Vec<Async>,
+}
+
+fn response_time_rate_monotonic(tasks: &mut [Cyclic]) -> Vec<f64> {
     use std::cmp::Ordering;
     tasks.sort_by(|t0: &Cyclic, t1: &Cyclic| if t0.period < t1.period {Ordering::Less}
         else if t0.period == t1.period {Ordering::Equal} else {Ordering::Greater});
@@ -32,12 +70,32 @@ fn response_time_rate_monotonic(mut tasks: Vec<Cyclic>) -> Vec<f64> {
     }).collect::<Vec<_>>()
 }
 
+fn response_time_rate_monotonic_load_save(tasks: &mut [Cyclic], load: f64, save: f64) -> Vec<f64> {
+    use std::cmp::Ordering;
+    tasks.sort_by(|t0: &Cyclic, t1: &Cyclic| if t0.period < t1.period {Ordering::Less}
+        else if t0.period == t1.period {Ordering::Equal} else {Ordering::Greater});
+    tasks.iter().enumerate().map(|(i, t)| {
+        let basis = t.task.wcet + load + save;
+        let mut cw = basis;
+        loop {
+            let nw = basis + tasks[0..i].iter().map(|tj| (cw/tj.period).ceil() *
+                (tj.task.wcet + load + save)).sum::<f64>();
+            if nw == cw {
+                return nw;
+            }
+            cw = nw;
+        }
+    }).collect::<Vec<_>>()
+}
+
 fn main() {
-    println!("Response times: {:?}", response_time_rate_monotonic(
-        vec![
+    println!("Response times: {:?}", response_time_rate_monotonic_load_save(
+        &mut vec![
             Cyclic::new(3.0, 7.0),
             Cyclic::new(3.0, 12.0),
             Cyclic::new(5.0, 20.0),
-        ]
+        ],
+        0.5,
+        0.5,
     ));
 }
